514dd8d495ea94e8aa10f940c6a82459b8fdca5d
Nadeem Parokkot <nadeemparokkot@gmail.com>
2024-08-27 16:06:46 +0530

    created a basic user register form next have to setup API endpoind for User registration

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
-import TextField from '@mui/material/TextField';
+import { TextField } from '@mui/material/';
+import { Button } from '@mui/material';
 
 function Register() {
-  return (
-    <div className='container text-center'>
-        <h1>Register</h1>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-    </div>
-  )
+    return (
+        <>
+        <div className='container text-center'>
+            <h1>Register</h1>
+            <div className='mt-3'>
+                <TextField id="email" type='email' label="Email" variant="outlined" />
+            </div>
+            <div className='mt-3'>
+                <TextField id="first_name" type="text" label="First name" variant="outlined" />
+            </div>
+            <div className='mt-3'>
+                <TextField id="last_name" type="text" label="Last name" variant="outlined" />
+            </div>
+            <div className='mt-3'>
+                <TextField id="password" type='password' label="Password" variant="outlined" />
+            </div>
+            <div className='mt-3'>
+                <Button variant="contained">Submit</Button>
+            </div>
+        </div>
+        </>   
+    )
 }
 
 export default Register
