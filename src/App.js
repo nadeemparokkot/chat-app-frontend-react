f2e4c351a1ad75eaec5553ccae4c10fde64ef804
Nadeem Parokkot <nadeemparokkot@gmail.com>
2024-08-28 15:02:51 +0530

    created a Navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Login from "./Components/Login";
+import Navigate from "./Components/Navigate";
 import Register from "./Components/Register";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
+      <Navigate/>
       <Routes>
         <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
         <Route path='/register' element={<Register />}></Route>
       </Routes>
     </BrowserRouter>
